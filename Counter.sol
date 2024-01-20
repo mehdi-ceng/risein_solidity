8295adb7cd9f0b000815d0042eb4cf84c6fc7c6f
Mehdi Maharramov <mehdi.maharramov1@gmail.com>
2024-01-20 11:20:47 +0400

    Create Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+
+contract Example {
+
+
+    address owner;
+
+
+    struct Counter {
+        uint number;
+        string description;
+    }
+
+
+    Counter counter;
+
+    
